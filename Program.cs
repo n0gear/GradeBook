52e62d9778b3412d47bc7fca92074f1f29dc9edc
Kristian Halonen <kristian@hilla.it>
2020-01-17 22:22:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Kristian's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.7);
+            
+            var stats = book.GetStatistics();
+            Console.WriteLine($"The Average is: {stats.Average:N2}");
+            Console.WriteLine($"The High is: {stats.High:N2}");
+            Console.WriteLine($"The Low is: {stats.Low:N2}");
+        }
+    }
+}
