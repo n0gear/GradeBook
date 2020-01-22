a163ecba356eaa6a18a404d3342fe2ac76892e75
Kristian Halonen <kristian@hilla.it>
2020-01-22 16:33:41 +0200

    Jumping with break and cont

--- Book.cs
+++ Book.cs
@@ -13,7 +13,15 @@ namespace GradeBook
         }
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
+                
         }
 
         public Statistics GetStatistics()
@@ -24,12 +32,11 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            for(var index = 0; index < grades.Count; index++)
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-
-                result.Average += grade;
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
             }
             result.Average /= grades.Count;
 
